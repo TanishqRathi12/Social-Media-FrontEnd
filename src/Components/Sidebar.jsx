a8f0b64766e1caf2595c4ede5ced7673e6e533a0
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-07-28 15:29:12 +0530

    Bugs Fixed

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,35 +13,55 @@ export const Sidebar = () => {
     <div>
       <div className="sm:hidden fixed top-4 left-4 z-50">
         <button onClick={handleToggle}>
-          {isOpen ? <FaTimes className="text-white" size={24} /> : <FaBars className="text-white" size={24} />}
+          {isOpen ? (
+            <FaTimes className="text-white" size={24} />
+          ) : (
+            <FaBars className="text-white" size={24} />
+          )}
         </button>
       </div>
       <div className="hidden sm:flex flex-col w-24 sm:w-64 min-h-screen fixed bg-gray-600">
         <nav className="flex-grow">
           <ul className="space-y-2">
             <Link to="/">
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Home</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Home
+              </li>
             </Link>
             <Link to="/Profile">
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Profile</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Profile
+              </li>
             </Link>
             <Link to="/Explore">
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Explore</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Explore
+              </li>
             </Link>
           </ul>
         </nav>
       </div>
-      <div className={`fixed top-0 left-0 w-40 h-full bg-gray-600 z-40 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 sm:hidden`}>
+      <div
+        className={`fixed top-0 left-0 w-40 h-full bg-gray-600 z-40 transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 sm:hidden`}
+      >
         <nav className="flex-grow pt-8">
           <ul className="space-y-2">
             <Link to="/" onClick={handleToggle}>
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Home</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Home
+              </li>
             </Link>
             <Link to="/Profile" onClick={handleToggle}>
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Profile</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Profile
+              </li>
             </Link>
             <Link to="/Explore" onClick={handleToggle}>
-              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">Explore</li>
+              <li className="text-white p-4 hover:bg-gray-300 hover:text-black">
+                Explore
+              </li>
             </Link>
           </ul>
         </nav>
