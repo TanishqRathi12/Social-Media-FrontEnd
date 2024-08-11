19b4bc7f6e716d923a6880eddef8e460bec8d06a
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-08-12 00:16:27 +0530

    bug fixed

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,10 +1,10 @@
 import instanceAxios from 'axios';
 
-const axios = instanceAxios.create({
-    baseURL: 'https://social-media-backend-dusky.vercel.app/api/',
+
+export const axios = instanceAxios.create({
+    baseURL: 'https://social-media-backend-dusky.vercel.app/',
     headers: {
         'Content-Type': 'application/json',
     },
-});
-
+})
 export default axios;
