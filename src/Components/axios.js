b6f029f5a719978ca6934896eb3bc11f9b9b1514
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-08-12 18:35:08 +0530

    URL updated

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -2,7 +2,7 @@ import instanceAxios from 'axios';
 
 
 export const axios = instanceAxios.create({
-    baseURL: 'https://social-media-backend-ten-sigma.vercel.app',
+    baseURL: 'https://social-media-backend-h61d.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
