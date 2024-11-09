7d72c63764174f8acb7063c488c30deb5ee91920
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-11-09 20:58:48 +0530

    Profile page is now more optimized

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import UserProfile from "../Components/UserProfile";
 import Loading from "../Components/LoadingHocs";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import axios from "../Components/axios";
+import _ from 'lodash'
+
 
 function Profile() {
   const UserProfileWithLoading = Loading(UserProfile);
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
+  const postsRef = useRef(null);
+  const Profile = useRef(null);
   const [userData, setUserData] = useState({
     name: "",
     image: "",
@@ -16,6 +20,16 @@ function Profile() {
     followerCount: 0,
     followingCount: 0,
   });
+  const dataHasChanged = (result) => {
+    const hasChanged = !_.isEqual(postsRef.current,result)
+    console.log("Data comparison for posts:", hasChanged,postsRef.current, result);
+    return hasChanged;
+  };
+  const ProfileHasChanged = (result) => {
+    const hasChanged = !_.isEqual(Profile.current,result);
+    console.log("Data comparison for Profile:", hasChanged,Profile.current, result);
+    return hasChanged;
+  }
   useEffect(() => {
     const fetchUserDataAndPost = async () => {
       const token = localStorage.getItem("token");
@@ -27,43 +41,54 @@ function Profile() {
             Authorization: `Bearer ${token}`,
           },
         });
-        try {
-          const response2 = await axios.get(`/getPostById/${userId}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
+        if (!Profile.current || ProfileHasChanged(response.data)) {
+          console.log("Profile Data is Fetched")
+          Profile.current = response.data;
+          setUserData({
+            name: response.data.username,
+            image: response.data.ProfilePicture,
+            Bio: response.data.Bio,
+            followerCount: response.data.followers,
+            followingCount: response.data.following,
           });
-          setPosts(response2.data);
-        } catch (error) {
-          console.error("Error fetching user Post:",error);
+        } else {
+          console.log("Data is not fetched thanks UseRef")
         }
-        setUserData({
-          name: response.data.username,
-          image: response.data.ProfilePicture,
-          Bio: response.data.Bio,
-          followerCount: response.data.followers,
-          followingCount: response.data.following,
-        });
       } catch (error) {
         console.error("Error fetching user data:", error);
       } finally {
         setLoading(false);
       }
     };
-
-    // const fetchPostById = async () => {
-    //   const token = localStorage.getItem("token");
-    //   const decoded = jwtDecode(token);
-    //   const userId = decoded.id;
-    //   try {
-
-    //     //console.log(response.data)
-    //   } catch (error) {
-    //     console.error("Error Fetching the posts", error);
-    //   }
-    // };
-    // fetchPostById();
     fetchUserDataAndPost();
+  }, []);
+  useEffect(() => {
+    const fetchPostById = async () => {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token);
+      const userId = decoded.id;
+      try {
+        const response2 = await axios.get(`/getPostById/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const result = response2.data;
+        if (!postsRef.current || dataHasChanged(result)) {
+          console.log("posts are fetched or changed");
+          postsRef.current = result;
+          setPosts(result);
+        } else {
+          console.log("Not fetched again Thanks UseRef");
+        }
+      } catch (error) {
+        console.error("Error Fetching the posts", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPostById();
   }, []);
 
   return (
