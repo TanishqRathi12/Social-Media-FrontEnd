419141a935c9572df1c4e06874649bfc2f715757
TanishqRathi12 <rathitanishq2022@gmail.com>
2025-01-16 08:42:19 +0530

    backend link updated

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -2,7 +2,7 @@ import instanceAxios from 'axios';
 
 
 export const axios = instanceAxios.create({
-    baseURL: 'https://social-media-backend-production-d73b.up.railway.app/',
+    baseURL: 'https://social-media-backend-production-60e2.up.railway.app/',
     //baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json',
