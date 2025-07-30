60326e9341c1ce971910336eb5989ea5ec2192b5
TanishqRathi12 <rathitanishq2022@gmail.com>
2025-07-30 16:35:52 +0530

    backend link changed

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -2,8 +2,8 @@ import instanceAxios from 'axios';
 
 
 export const axios = instanceAxios.create({
-    // baseURL: 'https://social-media-backend-tyd2.onrender.com/',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://social-media-backend-tyd2.onrender.com/',
+    // baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json',
     },
